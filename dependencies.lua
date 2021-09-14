6483a0a589e3ac8a738149d2d0fae3f79318292d
alex-boulay <alexandre.boulay59@gmail.com>
2021-09-14 22:10:54 +0200

    dependencies modifications

--- dependencies.lua
+++ dependencies.lua
@@ -1,7 +1,9 @@
 Class = require 'class'
 local curr_dir=love.filesystem.getRealDirectory("")
 package.path = package.path .. ";"..curr_dir .."../MathStructs/?.lua"
+package.path = package.path .. ";"..curr_dir .."../Lighting/?.lua"
+
 require 'MathStructs'
 require 'LightSource'
-require 'TileObs'
 require 'Lighting'
+require 'TileObs'
